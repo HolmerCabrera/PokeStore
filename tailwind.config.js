7a3a12d7a38fb0aece6cd3688496743e20c54ba0
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-05-11 20:56:46 -0500

    Estructura del proyecto, personalizacion y carpeta img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      },
+      fontFamily: {
+        Monserrat: ['Monserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
