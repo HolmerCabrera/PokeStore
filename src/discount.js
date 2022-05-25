5e7757a9bbd977489981479516a4e3d4e47abd7f
HolmerCabrera <holmercabrera@gmail.com>
2022-05-24 20:54:54 -0500

    Se agregaron baras de texto

--- src/discount.js
+++ src/discount.js
@@ -1,1 +1,23 @@
-alert("Pagina de Descuentos");
+
+function CalcularPrecioConDescuento(ValorProducto, ValorDescuento){
+
+
+    const ValorConDescuento =  ValorProducto-(ValorProducto*ValorDescuento/100);
+
+    return ValorDescuento 
+
+
+}
+
+
+
+function onClickCalcularDescuento() {
+    
+    const ValorProducto = document.getElementById("ValorProducto").value;
+    const ValorDescuento = document.getElementById("ValorDescuento").value;
+    const ValorConDescuento = CalcularPrecioConDescuento(ValorProducto,ValorDescuento);
+    console.log(ValorConDescuento);
+
+}
+
+
