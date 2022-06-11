ad1bb108e70297ff212ece28cfa2a3b416abe489
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-06-11 16:33:35 -0500

    Se agrega carpeta public y se ordenan los archivos alli para el deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  content: ["./src/index.html", "./src/**/*.{html,js}"],
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'PokemonCristal': "url('../img/PokemonCristal.png')",
-        'PokeballAndCards': "url('../img/PokeballAndCards.jpg')",
-        'PokemonLegendArceus': "url('../img/PokemonLegendArceus.jpg')",
-        'LetsGoPikachuAndEevee': "url('../img/LetsGoPikachuAndEevee.png')",
-        'PokemonSwordShield': "url('../img/PokemonSwordShield.png')",
-        'UserPicture': "url('../img/UserPicture.jpg')",
-        'MeowthMoney': "url('../img/MeowthMoney.jpg')",
+        'PokemonCristal': "url('../public/img/PokemonCristal.png')",
+        'PokeballAndCards': "url('../public/img/PokeballAndCards.jpg')",
+        'PokemonLegendArceus': "url('../public/img/PokemonLegendArceus.jpg')",
+        'LetsGoPikachuAndEevee': "url('../public/img/LetsGoPikachuAndEevee.png')",
+        'PokemonSwordShield': "url('../public/img/PokemonSwordShield.png')",
+        'UserPicture': "url('../public/img/UserPicture.jpg')",
+        'MeowthMoney': "url('../public/img/MeowthMoney.jpg')",
       },
       backgroundColor: theme => ({
         ...theme('colors'),
